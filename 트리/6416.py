4ff162b264be47471cc8787cfd194da5b2ba637d
cheon4050 <ehdcjs159@gmail.com>
2021-07-28 00:21:57 +0900

    2021/07/28

---
+++ 트리/6416.py
@@ -0,0 +1,41 @@
+stop = True
+cnt = 1
+while stop:
+    treearr = []
+    while True:
+        arr = input()
+        if not arr:
+            continue
+        if arr[0] == "-":
+            stop = False
+            break
+        elif arr[-3:] == "0 0":
+            for i in arr.split("  ")[:-1]:
+                treearr.append(i)
+            break
+        else:
+            for i in arr.split("  "):
+                treearr.append(i)
+    if stop == False:
+        break
+    if not treearr:
+        print("Case " + str(cnt) + " is a tree.")
+        cnt += 1
+        continue
+    treeoutchack = []
+    treeincheck = []
+    for i in treearr:
+        t_in, t_out = i.split(" ")
+        if t_out in treeoutchack:
+            print("Case " + str(cnt) + " is not a tree.")
+            cnt += 1
+            break
+        treeoutchack.append(t_out)
+        treeincheck.append(t_in)
+    else:
+        if len(set(treeincheck) - set(treeoutchack)) != 1:
+            print("Case " + str(cnt) + " is not a tree.")
+            cnt += 1
+        else:
+            print("Case " + str(cnt) + " is a tree.")
+            cnt += 1
